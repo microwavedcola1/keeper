da7db9ae2dba52e5d4552013665c09ab043bfbb4
microwavedcola1 <microwavedcola@gmail.com>
2022-01-12 17:26:11 +0100

    add comment
    Signed-off-by: microwavedcola1 <microwavedcola@gmail.com>

--- programs/keeper/src/state/job.rs
+++ programs/keeper/src/state/job.rs
@@ -7,6 +7,9 @@ use anchor_lang::prelude::*;
 pub struct Job {
     pub program: Pubkey,
     pub credits_mint: Pubkey,
+    // todo: unsure how about bundling ix_tag, and later checking it, technically programs are free
+    // to decode incoming ix data as they see fit, it doesnt need to be first 4 bytes, though
+    // that is the convention
     pub ix_tag: u32,
     // todo padding
 }
